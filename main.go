ab2ad208899a2292023af7ed46404e95d9b8d214
this-is-mjk <mjlivetowork@gmail.com>
2024-04-04 20:14:18 +0530

    done

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"flag"
 	"fmt"
 	"github.com/gofor-little/env"
@@ -10,9 +13,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
+	"time"
 )
 
 func setEnv(sourceFile string, destiantionFile string, encryptionKey string) {
@@ -111,13 +112,13 @@ func copyFile(sourceFile string, destiantionFile string, encryption bool) {
 		}
 		// ecrypt file
 		cipherText := gcm.Seal(nonce, nonce, plainText, nil)
-		
+
 		// Writing ciphertext file
 		err = os.WriteFile(destiantionFile, cipherText, 0777)
 		if err != nil {
 			log.Fatalf("write file err: %v", err.Error())
 		}
-	}else {
+	} else {
 		_, err = io.Copy(destination, source)
 		if err != nil {
 			log.Fatal("Error104:", err)
@@ -125,15 +126,77 @@ func copyFile(sourceFile string, destiantionFile string, encryption bool) {
 	}
 }
 func ittrateOverDir(path string, d fs.DirEntry, backUpDir string, sourceDir string, encryption bool) {
+	currentTime := time.Now()
+	formattedTime := currentTime.Format("2006-01-02 15:04:05")
+	// Create intermediate directories if they don't exist
+	err1 := os.MkdirAll(filepath.Dir(backUpDir+"/log.txt"), os.ModePerm)
+	if err1 != nil {
+		log.Fatal("Error102: ", err1)
+		return
+	}
+	// Open the log file in append mode
+	file, err := os.OpenFile(backUpDir+"/log.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	// Append to log file
+	_, err = file.WriteString(formattedTime +  " ecryption? " + fmt.Sprintf("%t", encryption) + " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	if d.IsDir() {
-		fmt.Printf("DIRECTORY DETECTED %s\n", d.Name())
+		log.Printf("DIRECTORY DETECTED %s\n", d.Name())
+		// Append to log file
+		_, err = file.WriteString("DIRECTORY DETECTED " + d.Name() + "\n")
+		if err != nil {
+			log.Fatal(err)
+		}
 		return // skip it
 	} else {
 		trimmed := strings.TrimPrefix(path, sourceDir)
 		copyFile(path, backUpDir+trimmed, encryption)
 		fmt.Print(".")
+
+		// Append to log file
+		_, err = file.WriteString("Copying file " + d.Name() + "\n")
+		if err != nil {
+			log.Fatal(err)
+		}
+
 	}
 }
+func decryptFile(key string, finalName string, encryptedFilePath string) {
+		// Reading ciphertext file
+		cipherText, err := os.ReadFile(encryptedFilePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Creating block of algorithm
+		block, err := aes.NewCipher([]byte(key))
+		if err != nil {
+			log.Fatalf("cipher err: %v", err.Error())
+		}
+		// Creating GCM mode
+		gcm, err := cipher.NewGCM(block)
+		if err != nil {
+			log.Fatalf("cipher GCM err: %v", err.Error())
+		}
+		// Deattached nonce and decrypt
+		nonce := cipherText[:gcm.NonceSize()]
+		cipherText = cipherText[gcm.NonceSize():]
+		plainText, err := gcm.Open(nil, nonce, cipherText, nil)
+		if err != nil {
+			log.Fatalf("decrypt file err: %v", err.Error())
+		}
+		// Writing decryption content
+		err = os.WriteFile(finalName, plainText, 0777)
+		if err != nil {
+			log.Fatalf("write file err: %v", err.Error())
+		}
+}
+	
 func main() {
 	// flags
 	SourcePtr := flag.String("cSP", "", "To set source directory path, include -cSP=")
@@ -142,6 +205,7 @@ func main() {
 	printPathPtr := flag.Bool("Printpaths", false, "To print the current set paths, include -Printpaths")
 	printEKPtr := flag.Bool("PrintEK", false, "To print the current set Key, include -PrintEK")
 	encryption := flag.Bool("E", false, "To encrypt and back-it-up, include -E")
+	decrypt := flag.Bool("D", false, "To decrypt, include -D")
 	flag.Parse()
 
 	// Initial Paths initaisation
@@ -162,9 +226,39 @@ func main() {
 		fmt.Printf("Encryption Key: %s\n", EK)
 		return
 	}
+	// decrypt
+	if *decrypt {
+		// key
+		fmt.Print("Enter the key: ")
+		var key string
+		_, err := fmt.Scanf("%s\n", &key)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
 
+		// final file name
+		fmt.Print("Enter the final file name: ")
+		var finalFileName string
+		_, err = fmt.Scanf("%s\n", &finalFileName)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		// encrypted file path
+		fmt.Print("Enter the encrypted file path: ")
+		var encryptedFilePath string
+		_, err = fmt.Scanf("%s\n", &encryptedFilePath)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		decryptFile(key, finalFileName, encryptedFilePath)
+		return
+	}
 	// ittrate over the dir
-	fmt.Print("working...\n")
+	log.Print("working...\n")
 	err := filepath.WalkDir(sourceDir, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err // Return the error if encountered during traversal
