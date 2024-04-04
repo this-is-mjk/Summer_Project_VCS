780ae98afee63e9729ee58a0a9803c05f2763254
this-is-mjk <mjlivetowork@gmail.com>
2024-04-04 18:19:54 +0530

    encryption done

--- main.go
+++ main.go
@@ -10,9 +10,12 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 )
 
-func setEnv(sourceFile string, destiantionFile string) {
+func setEnv(sourceFile string, destiantionFile string, encryptionKey string) {
 	// Get the current working directory
 	cwd, _ := os.Getwd()
 	// Load an .env file and set the key-value pairs as environment variables.
@@ -29,6 +32,12 @@ func setEnv(sourceFile string, destiantionFile string) {
 	if destiantionFile != "" {
 		// If not, write the key-value pair
 		if err := env.Write("DESTINATION_PATH", destiantionFile, ".env", true); err != nil {
+			log.Fatal("Error203:", err)
+		}
+	}
+	if encryptionKey != "" {
+		// If not, write the key-value pair
+		if err := env.Write("ENCRYPTION_KEY", encryptionKey, ".env", true); err != nil {
 			log.Fatal("Error203:", err)
 		}
 	}
@@ -53,7 +62,7 @@ func getEnv() (string, string) {
 	}
 	return sourceFile, destiantionFile
 }
-func copyFile(sourceFile string, destiantionFile string) {
+func copyFile(sourceFile string, destiantionFile string, encryption bool) {
 	// Open the source file
 	source, err := os.Open(sourceFile)
 	if err != nil {
@@ -74,34 +83,81 @@ func copyFile(sourceFile string, destiantionFile string) {
 	}
 	defer destination.Close()
 	// Write the contents of source to the destination file
-	_, err = io.Copy(destination, source)
-	if err != nil {
-		log.Fatal("Error104:", err)
+	if encryption {
+		// Reading key
+		EK, err1 := env.MustGet("ENCRYPTION_KEY")
+		if err1 != nil {
+			log.Fatal("Please set the ENCRYPTION_KEY by -cEK")
+		}
+		// Reading plaintext file
+		plainText, err := os.ReadFile(sourceFile)
+		if err != nil {
+			log.Fatalf("read file err: %v", err.Error())
+		}
+		// Creating block-cypher of algorithm
+		block, err := aes.NewCipher([]byte(EK))
+		if err != nil {
+			log.Fatalf("cipher err: %v", err.Error())
+		}
+		// Creating GCM mode
+		gcm, err := cipher.NewGCM(block)
+		if err != nil {
+			log.Fatalf("cipher GCM err: %v", err.Error())
+		}
+		// Generating random nonce
+		nonce := make([]byte, gcm.NonceSize())
+		if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+			log.Fatalf("nonce  err: %v", err.Error())
+		}
+		// ecrypt file
+		cipherText := gcm.Seal(nonce, nonce, plainText, nil)
+		
+		// Writing ciphertext file
+		err = os.WriteFile(destiantionFile, cipherText, 0777)
+		if err != nil {
+			log.Fatalf("write file err: %v", err.Error())
+		}
+	}else {
+		_, err = io.Copy(destination, source)
+		if err != nil {
+			log.Fatal("Error104:", err)
+		}
 	}
 }
-func ittrateOverDir(path string, d fs.DirEntry, backUpDir string, sourceDir string) {
+func ittrateOverDir(path string, d fs.DirEntry, backUpDir string, sourceDir string, encryption bool) {
 	if d.IsDir() {
 		fmt.Printf("DIRECTORY DETECTED %s\n", d.Name())
 		return // skip it
 	} else {
 		trimmed := strings.TrimPrefix(path, sourceDir)
-		copyFile(path, backUpDir+trimmed)
+		copyFile(path, backUpDir+trimmed, encryption)
 		fmt.Print(".")
 	}
 }
 func main() {
-	SourcePtr := flag.String("cSP", "", "To set source directory path")
-	BackUPPtr := flag.String("cBP", "", "To set backUp directory path")
-	printPathPtr := flag.Bool("PrintPath", false, "To print the current set paths")
-	
+	SourcePtr := flag.String("cSP", "", "To set source directory path, include -cSP=")
+	BackUPPtr := flag.String("cBP", "", "To set backUp directory path, include -cBP=")
+	encrytpionKeyPtr := flag.String("cEK", "", "To set encryption key, include -cEK=")
+	printPathPtr := flag.Bool("Printpaths", false, "To print the current set paths, include -Printpaths")
+	printEKPtr := flag.Bool("PrintEK", false, "To print the current set Key, include -PrintEK")
+	encryption := flag.Bool("E", false, "To encrypt and back-it-up, include -E")
 	flag.Parse()
 	// Initial Paths initaisation
-	setEnv(*SourcePtr, *BackUPPtr)
-	// Get the env variables
+	setEnv(*SourcePtr, *BackUPPtr, *encrytpionKeyPtr)
+	// Get the variables
 	sourceDir, backupDir := getEnv()
 	// flag to print the current path
 	if *printPathPtr {
-		fmt.Printf("Source Directory: %s\nBackUp Directory: %s\n", sourceDir, backupDir);
+		fmt.Printf("Source Directory: %s\nBackUp Directory: %s\n", sourceDir, backupDir)
+		return
+	}
+	if *printEKPtr {
+		// Get an environment variable's value, receiving an error if it is not set or is empty.
+		EK, err1 := env.MustGet("ENCRYPTION_KEY")
+		if err1 != nil {
+			log.Fatal("Please set the ENCRYPTION_KEY by -cEK")
+		}
+		fmt.Printf("Encryption Key: %s\n", EK)
 		return
 	}
 	// ittrate over the dir
@@ -110,7 +166,7 @@ func main() {
 		if err != nil {
 			return err // Return the error if encountered during traversal
 		}
-		ittrateOverDir(path, d, backupDir, sourceDir)
+		ittrateOverDir(path, d, backupDir, sourceDir, *encryption)
 		return nil
 	})
 	if err != nil {
